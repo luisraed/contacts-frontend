ed3384f587b0df353811d5f3030654053199d6ac
Luis R <luisraed@gmail.com>
2021-03-27 16:32:31 -0300

    Adding the contacts component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/models/contact.ts
@@ -0,0 +1,12 @@
+export class Contact {
+    public salutation: string = "";
+    public firstName: string = "";
+    public lastName: string = "";
+    public addressLine1: string = "";
+    public addressLine2: string = "";
+    public city: string = "";
+    public state: string = "";
+    public zipCode: string = "";
+    public emailAddress: string = "";
+    public telephoneNumber: string = "";
+}
